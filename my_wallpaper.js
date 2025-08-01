7d23d71228ad05d7024f1bc836054ce79cd41f86
FinnHyndman <finnhyndman@gmail.com>
2025-08-01 15:54:02 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -13,46 +13,47 @@ let colWhite = [255,255,255]
 //your parameter variables go here!
 
 //background variables
-let backgroundColour = colYellow
+//background variables
+let backgroundColour = colBlue
 let showAmbientStars = true
 
 //star cluster colours
-let starClustersColour = colBlack
+let starClustersColour = colLightYellow
 
 //Trio star cluster variables
 let starClusterTrioShow = true
-let starClusterTrioX = 150
-let starClusterTrioY = 20
+let starClusterTrioX = 30
+let starClusterTrioY = 30
 
 //Duo star cluster variables
 let starClusterDuoShow = true
-let starClusterDuoX = 40
-let starClusterDuoY = 115
+let starClusterDuoX = 20
+let starClusterDuoY = 180
 
 //Planet variables
 let planetsShow = true
-let largePlanetColour = colBlack
-let mediumPlanetColour = colBlack
-let smallPlanetColour = colBlack
-let planetRingColour = colBlack
+let largePlanetColour = colPink
+let mediumPlanetColour = colYellow
+let smallPlanetColour = colOrange
+let planetRingColour = colWhite
 let planetsSize = 1
-let planetsX = 90
-let planetsY = 60
+let planetsX = 40
+let planetsY = 70
 
 //UFO variables
-let ufoShow = false
-let ufoX = 55
+let ufoShow = true
+let ufoX = 110
 let ufoY = 120
-let ufoSize = 50
+let ufoSize = 30
 
-//Shift constellation horizontally (will only show when set between 44 and 106)
-let constellationX = 90
+//Shift constellation horizontally
+let constellationX = 100
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -115,9 +116,10 @@ planetRadiate(planetsX+100,planetsY+10,planetsSize+2)
   planetRadiate(planetsX+20,planetsY+40,planetsSize+1)
   }
 }
+
 // Constellation Draw 
  if (constellationX > 43 && constellationX < 107) {
-    constellation(); 
+  constellation(); 
 }
 
 //UFO draw
